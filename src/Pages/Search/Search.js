15a05580d90eb33a303ad82c884ba3b3862b39b4
filmons <filmonseare@gmail.com>
2021-09-03 18:10:28 +0200

    fetch search part and display on the page 🚀🚀🚀

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,36 +1,107 @@
-import { Button, createTheme, TextField } from "@material-ui/core";
-import { useState } from "react";
+import { Button, createTheme, Tab, Tabs, TextField } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "@material-ui/styles";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
+import axios from "axios";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 function Search() {
 	const [type, settype] = useState(0);
+    const [searchText, setSearchText] = useState("");
+    const [page, setPage] = useState(1);
+    const [content, setContent] = useState([]);
+    const [numOfPages, setNumOfPages] = useState();
 
 	const darckTheme = createTheme({
 		palette: {
-			type: "dark",
-		},
+            type: "dark",
+            primary: {
+              main: "#fff",
+            },
+          },
 	});
+
+
+    const fetchSearch = async () => {
+        try {
+          const { data } = await axios.get(
+            `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+              process.env.REACT_APP_API_KEY
+            }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+          );
+          setContent(data.results);
+          setNumOfPages(data.total_pages);
+          // console.log(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    
+      useEffect(() => {
+        window.scroll(0, 0);
+        fetchSearch();
+        // eslint-disable-next-line
+      }, [type, page]);
+    
 
 	return (
 		<div>
 			<ThemeProvider theme={darckTheme}>
-				<div style={{ display:"flex"}}>
+				<div style={{ display: "flex", margin:"15px 0"}}>
 					<TextField
 						style={{ flex: 1 }}
 						className="searchBox"
 						label="Search"
 						variant="filled"
-						//onChange={(e) => setSearchText(e.target.value)}
+						onChange={(e) => setSearchText(e.target.value)}
 					/>
-                    <Button 
-                    variant="contained"
+					<Button 
+                    variant="contained" 
                     style={{ marginLeft: 10 }}
+                    onClick={fetchSearch}
                     
-                    > <SearchIcon/> </Button>
-                    
+                    >
+						
+						<SearchIcon />
+					</Button>
 				</div>
+
+            <Tabs 
+            value={type} 
+            indicatorColor="primary" 
+            textColor="primary"
+            onChange={(event, newValue) => {
+                settype(newValue);
+                setPage(1);
+              }}
+              style={{paddingBottom: 5}}
+            >
+            <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
+            </Tabs>
 			</ThemeProvider>
+            <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={type ? "tv" : "movie"}
+              vote_average={c.vote_average}
+            />
+          ))}
+        {searchText &&
+          !content &&
+          (type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
+
 		</div>
 	);
 }
