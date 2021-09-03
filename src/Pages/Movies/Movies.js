1e989132be5be7fc779c5d769e7e85f8c154a49b
filmons <filmonseare@gmail.com>
2021-09-03 16:07:36 +0200

    get all tv series 🚀🚀🚀

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -5,69 +5,59 @@ import SingleContent from "../../components/SingleContent/SingleContent";
 import Genres from "../../components/Genres";
 import useGenre from "../../hooks/useGenre";
 
-
-
-
-
 function Movies() {
+	const [content, setContent] = useState([]);
+	const [page, setPage] = useState(1); //par defolte pour que la page comence par 1
+	const [numOfPages, setnumOfPages] = useState(); //pour voir le numero de pade quon veux
+	const [selectedGenres, setSelectedGenres] = useState([]);
+	const [genres, setGenres] = useState([]);
+	const genreforURL = useGenre(selectedGenres);
 
-    const [content, setContent] = useState([])
-    const [page, setPage] = useState(1)//par defolte pour que la page comence par 1
-    const [numOfPages, setnumOfPages]= useState();//pour voir le numero de pade quon veux
-    const [selectedGenres, setSelectedGenres] = useState([])
-    const [genres, setGenres] = useState([])
-    const genreforURL=useGenre(selectedGenres)
+	const fetcheMovis = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+		);
+		setContent(data.results);
+		setnumOfPages(data.total_pages);
 
-    const fetcheMovis = async () => {
+		console.log(data);
+	};
+	useEffect(() => {
+		fetcheMovis();
+		// eslint-disable-next-line
+	}, [page, genreforURL]);
+	// call the function  genreforURL for uploding the page
 
-        const {data} = await axios.get (
-            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
-        
-        )
-        setContent(data.results)
-        setnumOfPages(data.total_pages)
-
-        console.log(data);
-    }
-    useEffect(() => {
-       fetcheMovis();
-       // eslint-disable-next-line
-    },[page, genreforURL])
-   // call the function  genreforURL for uploding the page
-
-    return (
-        <div>
-            <span className="pageTitle">Movies</span>
-            <Genres
-        type="movie"
-        selectedGenres={selectedGenres}
-        setSelectedGenres={setSelectedGenres}
-        genres={genres}
-        setGenres={setGenres}
-        setPage={setPage}
-      />
-            <div className='trending'>
-                 {content && content.map((c) => ( <SingleContent key={c.id} 
-                 id={c.id}
-                 poster={c.poster_path} 
-                 title={c.title || c.name} 
-                 date={c.first_air_date || c.release_date}  
-                 media_type="movie"
-                 vote_average={c.vote_average}
-
-                 
-                 
-                 />
-                  ))}
-
-             </div>
-             {numOfPages > 1 &&  
-             
-             <CustomPagination setPage={setPage} numOfPages={numOfPages}/>
-             }
-            
-        </div>
-    )
+	return (
+		<div>
+			<span className="pageTitle">Movies</span>
+			<Genres
+				type="movie"
+				selectedGenres={selectedGenres}
+				setSelectedGenres={setSelectedGenres}
+				genres={genres}
+				setGenres={setGenres}
+				setPage={setPage}
+			/>
+			<div className="trending">
+				{content &&
+					content.map((c) => (
+						<SingleContent
+							key={c.id}
+							id={c.id}
+							poster={c.poster_path}
+							title={c.title || c.name}
+							date={c.first_air_date || c.release_date}
+							media_type="movie"
+							vote_average={c.vote_average}
+						/>
+					))}
+			</div>
+			{numOfPages > 1 && (
+				<CustomPagination setPage={setPage} numOfPages={numOfPages} />
+			)}
+		</div>
+	);
 }
 
-export default Movies
+export default Movies;
--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,7 +1,63 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios";
+import Genres from "../../components/Genres";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import useGenre from "../../hooks/useGenre";
+
 function Series ()  {
+    const [content, setContent] = useState([]);
+	const [page, setPage] = useState(1); //par defolte pour que la page comence par 1
+	const [numOfPages, setnumOfPages] = useState(); //pour voir le numero de pade quon veux
+	const [selectedGenres, setSelectedGenres] = useState([]);
+	const [genres, setGenres] = useState([]);
+	const genreforURL = useGenre(selectedGenres);
+
+	const fetcheMovis = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+		);
+		setContent(data.results);
+		setnumOfPages(data.total_pages);
+
+		console.log(data);
+	};
+	useEffect(() => {
+		fetcheMovis();
+		// eslint-disable-next-line
+	}, [page, genreforURL]);
+	// call the function  genreforURL for uploding the page
+
     return (
         <div>
-            <span className="pageTitle">Series</span>
+            <span className="pageTitle">TV Series</span>
+            <p>f</p>
+            <Genres 
+				type="Tv"
+				selectedGenres={selectedGenres}
+				setSelectedGenres={setSelectedGenres}
+				genres={genres}
+				setGenres={setGenres}
+				setPage={setPage}
+			/>
+
+            <div className="trending">
+				{content &&
+					content.map((c) => (
+						<SingleContent
+							key={c.id}
+							id={c.id}
+							poster={c.poster_path}
+							title={c.title || c.name}
+							date={c.first_air_date || c.release_date}
+							media_type="tv"
+							vote_average={c.vote_average}
+						/>
+					))}
+			</div>
+			{numOfPages > 1 && (
+				<CustomPagination setPage={setPage} numOfPages={numOfPages} />
+			)}
         </div>
     )
 }
