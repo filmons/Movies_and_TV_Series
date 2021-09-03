78cb7f35155a67432df73ffa50ac7061b28d27d2
filmons <filmonseare@gmail.com>
2021-09-03 16:49:20 +0200

    searche part whit textField and createthemeProvider 🚀🚀🚀

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,10 +1,35 @@
+import { createTheme, TextField } from "@material-ui/core";
+import { useState } from "react";
+import { ThemeProvider } from "@material-ui/styles";
+
 function Search() {
-    return (
-        <div>
-            <span className="pageTitle">Searching</span>
-            
-        </div>
-    )
+	const [type, settype] = useState(0);
+
+
+
+    const darckTheme = createTheme({
+        palette:{
+            type:"dark"
+        }
+        
+        })
+
+	return (
+		<div>
+
+            <ThemeProvider theme={darckTheme}>
+
+
+			<TextField
+				style={{ flex: 1 }}
+				className="searchBox"
+				label="Search"
+				variant="filled"
+				//onChange={(e) => setSearchText(e.target.value)}
+			/>
+                </ThemeProvider>
+		</div>
+	);
 }
 
-export default Search
+export default Search;
--- src/Pages/Search/resurcepourSearch.js
+++ src/Pages/Search/resurcepourSearch.js
@@ -1,1 +1,3 @@
 /// utilmiser chips,   https://material-ui.com/components/chips/#chip
+
+/// TextFilde material ui ///https://material-ui.com/components/text-fields/#text-field
