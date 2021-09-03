db1dbe66e2657c1b453f6cbd5d617795341db9f4
filmons <filmonseare@gmail.com>
2021-09-03 17:14:26 +0200

    searchIcon

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,33 +1,36 @@
-import { createTheme, TextField } from "@material-ui/core";
+import { Button, createTheme, TextField } from "@material-ui/core";
 import { useState } from "react";
 import { ThemeProvider } from "@material-ui/styles";
+import SearchIcon from '@material-ui/icons/Search';
 
 function Search() {
 	const [type, settype] = useState(0);
 
-
-
-    const darckTheme = createTheme({
-        palette:{
-            type:"dark"
-        }
-        
-        })
+	const darckTheme = createTheme({
+		palette: {
+			type: "dark",
+		},
+	});
 
 	return (
 		<div>
-
-            <ThemeProvider theme={darckTheme}>
-
-
-			<TextField
-				style={{ flex: 1 }}
-				className="searchBox"
-				label="Search"
-				variant="filled"
-				//onChange={(e) => setSearchText(e.target.value)}
-			/>
-                </ThemeProvider>
+			<ThemeProvider theme={darckTheme}>
+				<div style={{ display:"flex"}}>
+					<TextField
+						style={{ flex: 1 }}
+						className="searchBox"
+						label="Search"
+						variant="filled"
+						//onChange={(e) => setSearchText(e.target.value)}
+					/>
+                    <Button 
+                    variant="contained"
+                    style={{ marginLeft: 10 }}
+                    
+                    > <SearchIcon/> </Button>
+                    
+				</div>
+			</ThemeProvider>
 		</div>
 	);
 }
